4c02b7c5eb4a6887b275a15a031ba99e1fbd2478
mariqdenevska <mariqdenevska@gmail.com>
2024-11-16 01:21:39 +0200

    Changes to be committed: 	modified:   Python basics.ipynb 	modified:   test.py

--- test.py
+++ test.py
@@ -1,30 +1,15 @@
-import random
-import string
-
-def generate_password(num_of_characters,password_strength):
-    password =""
-    if password_strength == "easy":
-        password = ''.join(random.choices(string.digits,k=num_of_characters))
-    elif password_strength == "medium":
-        password = ''.join(random.choices(string.ascii_letters + string.digits, k=num_of_characters))
-    elif password_strength == "hard":
-        password = ''.join(random.choices(string.ascii_letters+string.digits+string.punctuation, k=num_of_characters))
-    return password
-
-
-
-num_of_characters_input = int(input("Enter number of characters (must be bigger than 8): ")) 
-password_strength_input = input("Enter a password strength - easy, medium, or hard: ")
-
-if num_of_characters_input < 8:
-    print("Password must be longer than 8 characters.")
-elif password_strength_input not in ("easy", "medium", "hard"):
-    print("Password strength must be one of the following: easy, medium, or hard.")
-else:
-    password = generate_password(num_of_characters_input,password_strength_input)
-    print(f"The generated password is : {password}")
-
-
-
-
+def calc_person_age(age):
     
+    if age > 18 :
+        is_adult = True
+    else:
+        is_adult = False
+    return is_adult
+
+age_input = int(input("Enther your age : "))
+is_of_age = calc_person_age(age_input)
+
+if is_of_age:
+    print ("You are adult.")
+else:
+    print("You are under than 18.")
